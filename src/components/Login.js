ab52053113bce4869ec579bb857dbe7dae5a330c
shawchandankumar20 <shawchandankumar20@gmail.com>
2023-01-20 20:33:43 +0530

    Form handling Controlled components

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,37 +4,63 @@ class Login extends Component {
 
     constructor(props) {
         super(props);
-        this.emailInputRef = React.createRef();
-        this.passwordInputRef = React.createRef();
+        this.state = {
+            email: "",
+            password: "",
+        };
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({
+            password: e.target.value
+        });
     }
 
     handleFormSubmit = (e) => {
-        e.preventDefault();
-        console.log(this.emailInputRef);
-        console.log(this.passwordInputRef);
+        // e.preventDefault();
+        // console.log(this.emailInputRef);
+        // console.log(this.passwordInputRef);
     }
 
-    render() {
+  render() {
+    return (
+      <form className="login-form">
+        <span className="login-signup-header">Log In</span>
 
-        return (
-        <form className="login-form">
-            <span className="login-signup-header">Log In</span>
+        <div className="field">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+          />
+        </div>
 
-            <div className="field">
-                <input type="email" placeholder="Email" required ref={this.emailInputRef} />
-            </div>
-            
-            <div className="field">
-                <input type="password" placeholder="Password" required ref={this.passwordInputRef} />
-            </div>
+        <div className="field">
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={this.handlePasswordChange}
+            value={this.state.email}
+          />
+        </div>
 
-            <div className="field">
-                <button type="submit" onClick={this.handleFormSubmit} >Log In</button>
-            </div>
-        </form>
-        );
-    }
+        <div className="field">
+          <button type="submit" onClick={this.handleFormSubmit}>
+            Log In
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
-
 
 export default Login;
